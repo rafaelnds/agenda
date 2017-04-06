26fce937ea2e623149b7f2b8565a69cc3698ba85
Rafael Patricio <rafael@nds.com.br>
2017-04-06 09:58:32 -0400

    teste v4

--- teste5.php
+++ teste5.php
@@ -1,18 +1,36 @@
 <html>
    
    <head>
-      <title>Dynamic Function Calls</title>
+      <title>Tela Pessoa</title>
    </head>
    
    <body>
       
       <?php
-         function sayHello() {
-            echo "Hello<br />";
-         }
-         
-         $function_holder = "sayHello";
-         $function_holder();
+      require("bancoDados.php");
+      echo 'OLÁ!';
+         echo '<form method="post">';
+            echo'<h1>Tabela de Pessoas Cadastradas</h1>';
+            echo "<table border='3'><tr><th></th><th>ID</th><th>Nome</th><th>Telefone</th><th>Nome fantasia</th><th>Razao Social</th><th>EMAIL</th><th>Estado Civil</th><th>CPF</th><th>RG</th><th>CNPJ</th></tr>";
+            $array=bancoDados::imprimi();
+             for($i=0;$i<count($array); $i++){
+                error_log($array[$i]['ID']);
+                $pnome=$array[$i]['NOME'];  
+                $id = $array[$i]['ID'];	
+                $ec=$array[$i]['DESCRICAO'];
+                $empresa = $array[$i]['NOME_FANTASIA']; 
+                $razao = $array[$i]['RAZAO_SOCIAL'];
+                $telefone = $array[$i]['TELEFONE'];
+                $email = $array[$i]['EMAIL'];
+                $cpf = $array[$i]['CPF'];
+                $rg = $array[$i]['RG'];
+                $cnpj = $array[$i]['CNPJ'];
+           echo "<tr><td>$id</td><td>$pnome</td><td>$telefone</td><td>$empresa</td><td>$razao</td><td>$email</td><td>$ec</td><td> $cpf </td><td> $rg </td><td>$cnpj</td></tr>";
+          
+        }  
+        
+        echo "</table>";
+        echo '</form>';
       ?>
       
    </body>
